6dcb1016d3f6e15925759e33c1ead095b43a3b31
ridamgupta79-dev <ridamgupta79@gmail.com>
2026-07-15 23:35:43 +0530

    Time: 3 ms (54.89%), Space: 12.4 MB (53.87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mySqrt(self, x):
+
+        l = 1
+        r = x
+        ans = 0
+
+        while l <= r:
+
+            mid = (l + r) // 2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                ans = mid
+                l = mid + 1
+
+            else:
+                r = mid - 1
+
+        return ans
+        
+        
