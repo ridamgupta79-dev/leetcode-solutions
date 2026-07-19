7973b82898d490828c789c5e9d444d046e32f76e
ridamgupta79-dev <ridamgupta79@gmail.com>
2026-07-20 01:20:15 +0530

    Time: 0 ms (100%), Space: 19.8 MB (76.54%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverseString(self, s):
+
+        n = len(s)
+
+        i = 0
+        j = len(s) - 1
+
+        while i < j :
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        
+        return s
+
+        
+        
+        
