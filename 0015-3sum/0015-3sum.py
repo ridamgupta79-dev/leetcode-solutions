2e23c1a25b88e394247e6cfff22601ce2a6ca2c2
ridamgupta79-dev <ridamgupta79@gmail.com>
2026-07-21 22:59:05 +0530

    Time: 5497 ms (8%), Space: 20.5 MB (5.31%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        result = set() 
+        n = len(nums)
+
+        for first in range (0, n) :
+            my_set = set()
+            for second in range (first+1, n) :
+                third = 0 - nums[first] - nums[second]
+                if third in my_set :
+                    part = [nums[first], nums[second], third]
+                    part.sort()
+                    result.add(tuple(part))
+                my_set.add(nums[second])
+                    
+
+        return [list(x) for x in result]
