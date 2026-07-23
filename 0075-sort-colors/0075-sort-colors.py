b9fdf58dedba2fd3d2279ba3600b68991c452933
ridamgupta79-dev <ridamgupta79@gmail.com>
2026-07-23 21:03:11 +0530

    Time: 6 ms (7.64%), Space: 12.4 MB (54.96%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortColors(self, nums):
+
+        n = len(nums)
+
+        for i in range (0, n) :
+
+            for j in range (i+1, n) :
+
+                if nums[j] < nums[i] :
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        return nums
+            
+           
+        
+       
+        
+        
